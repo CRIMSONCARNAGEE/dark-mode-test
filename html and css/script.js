3e1411bc4ee15823a1530d9f6f52c384bae3ecd2
CRIMSONCARNAGEE <sohibaorazboyeva@gmail.com>
2024-11-23 21:46:15 +0500

    gg

---
+++ html and css/script.js
@@ -0,0 +1,11 @@
+// const ellogolink = document.querySelector(".site-header-title");
+
+// const elChangeLogoTextButton = document.querySelector = "change-logo-text-button";
+// elChangeLogoTextButton.addEventListener("click" , function () {
+//  ellogolink.textContent = "boshqa matn";
+// });
+const elDarkModeButton = document.querySelector("site-header-dark");
+
+elDarkModeButton.addEventListener("click", function(){
+  document.body.classList.add("dark-mode");
+});
