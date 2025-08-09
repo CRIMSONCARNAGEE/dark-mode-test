946b878d7a32a7e8703a0a660baa36c8f04dc3f0
Azizbek <azizbekurazboyev203@gmail.com>
2025-08-09 21:57:00 +0500

    Update script.js

--- html and css/script.js
+++ html and css/script.js
@@ -1,9 +1,4 @@
-// const ellogolink = document.querySelector(".site-header-title");
 
-// const elChangeLogoTextButton = document.querySelector = "change-logo-text-button";
-// elChangeLogoTextButton.addEventListener("click" , function () {
-//  ellogolink.textContent = "boshqa matn";
-// });
 // Dark mode
 const elDarkModeButton = document.querySelector("site-header-dark");
 
